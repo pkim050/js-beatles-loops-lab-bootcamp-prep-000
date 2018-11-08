83f128fe20b55b8652059e6ebe4d3b21e3c86d33
Patrick A Kim <pkim050@gmail.com>
2018-11-08 21:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musician, instrument) {
+  var array = {};
+  for (var i = 0; i < musician.length; i++) {
+    array.i = musician.i + " plays " + instrument.i;
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var j = facts.length;
+  while (j > 0) {
+    facts.j = facts.j + "!!!";
+    j--;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var array2 = {};
+  do {
+    array2 = "I love the Beatles!";
+    if (number === 14) {
+      array2 = "I love the Bea";
+    }
+    number++;
+  } while (number < 15)
+}

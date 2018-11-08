4f9b586a223405ace5924b7167f8d15bd0318152
Patrick A Kim <pkim050@gmail.com>
2018-11-08 22:03:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function theBeatlesPlay(musician, instrument) {
   var array = {};
   for (var i = 0; i < musician.length; i++) {
     array[i] = musician[i] + " plays " + instrument[i];
+    console.log(array[i]);
   }
   return array;
 }
